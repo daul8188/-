4a445037251f89117f1802a7a851e2a69d254ef1
daul8188 <2530911@sw.hs.kr>
2025-08-22 14:38:20 +0900

    Update gkg.py

--- gkg.py
+++ gkg.py
@@ -2,19 +2,20 @@ import streamlit as st
 
 st. title('gyukyulgyu')
 
-name = st. text input('이름:')
+name = st. text_input('이름:')
 choice = st.selectbox('고르세요:', ['남규현','이민규','김은결','김건호'])
 
 
 choice_data = {
   '남규현': {
         '특징': '방밸을 즐기며 코 사이즈가 가로 8cm, 세로 9cm를 소유함'
-
+  }
   '이민규': {
         '특징': '2반에서 왕따담당'
-
+}
   '김은결': {
         '특징': '예대도 강탈예정'
-  
+  } 
   '김건호': {
         '특징': '3도 화상입음'
+  }
